3b5bf8769d21987406b1d3be81463d100df3797f
zypheroq <158800359+zypheroq@users.noreply.github.com>
2025-07-27 00:39:44 -0700

    Update XPFarm.js

--- XPFarm.js
+++ XPFarm.js
@@ -1,8 +1,22 @@
-// Infecticide.js
+// XPFarm.js
 // Cracks and nukes all servers it can, then adds a file of your choice (filename), then runs it
 export async function main(ns) {
+	const visited = [];
+	function scanServers(host) {
+		const neighbors = ns.scan(host);
+		for (const neighbor of neighbors) {
+			if (!visited.includes(neighbor)) {
+				visited.push(neighbor);
+				scanServers(neighbor);
+			}
+		}
+	}
+
+	visited.push("home");
+	scanServers("home");
+
 	const filename = "xp/weaken.js"
-	const servers = ["home", "n00dles", "foodnstuff", "zer0", "phantasy", "sigma-cosmetics", "nectar-net", "CSEC", "neo-net", "computek", "crush-fitness", "summit-uni", "silver-helix", "the-hub", "zb-institute", "lexo-corp", "galactic-cyber", "rho-construction", "global-pharm", "syscore", "alpha-ent", "millenium-fitness", "aerocorp", "unitalife", "defcomm", "zb-def", "univ-energy", "snap-fitness", "omnia", "solaris", "zeus-med", "taiyang-digital", "microdyne", "vitalife", "omnitek", "b-and-a", "The-Cave", ".", "blade", "megacorp", "fulcrumassets", "powerhouse-fitness", "nova-med", "titan-labs", "run4theh111z", "helios", "deltaone", "icarus", "infocomm", "applied-energetics", "fulcrumtech", "kuai-gong", "stormtech", "4sigma", "nwo", "clarkinc", "ecorp", "johnson-ortho", "rothman-uni", "avmnite-02h", "catalyst", "aevum-police", "omega-net", "netlink", "I.I.I.I", "joesguns", "max-hardware", "hong-fang-tea", "harakiri-sushi", "iron-gym", "darkweb"]
+	const servers = visited
 	ns.print(ns.scan())
 	for (var num = 0; num < (servers.length); num++) {
 		var servername = servers[num]
