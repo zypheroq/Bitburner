9d93405da5f6488c3eb1b9e157af3c473a136865
zypheroq <158800359+zypheroq@users.noreply.github.com>
2025-08-01 19:22:17 -0700

    Update data.js
    fixed karma font
    moved karma below int
    lowered ram by using eval(document)

--- data.js
+++ data.js
@@ -6,6 +6,29 @@ export async function main(ns) {
 		if (num >= 1e3) return (num / 1e3).toFixed(1).replace(/\.0$/, '') + 'k';
 		return num.toString();
 	}
+
+	function calculateSkill(exp, mult = 1) {
+		const val = Math.floor(mult * (32 * Math.log(exp + 534.6) - 200));
+		return Math.max(1, val);
+	}
+
+	function calculateExp(skill, mult = 1) {
+		const s = Math.floor(skill);
+		let value = Math.exp((s / mult + 200) / 32) - 534.6;
+		return Math.max(0, value);
+	}
+
+	function calculateSkillProgress(exp, mult = 1) {
+		const currSkill = calculateSkill(exp, mult);
+		const nextSkill = currSkill + 1;
+
+		const baseExp = calculateExp(currSkill, mult);
+		const nextExp = calculateExp(nextSkill, mult);
+
+		const progress = ((exp - baseExp) / (nextExp - baseExp)) * 100;
+		return Math.min(Math.max(progress, 0), 100);
+	}
+
 	let avgIncomePerSecond
 	let samples = []
 	let hackText
@@ -20,9 +43,15 @@ export async function main(ns) {
 	let formattedKarma
 	let charismaBarRow
 	let p
+	let intText
+	let intPercent
+	let player
+
 	while (true) {
+		player = ns.getPlayer()
+
 		samples.push(ns.getServerMoneyAvailable("home"));
-		if(samples.length >= 60) {
+		if (samples.length >= 60) {
 			samples.shift()
 		}
 		if (samples.length > 1) {
@@ -34,38 +63,44 @@ export async function main(ns) {
 			avgIncomePerSecond = avgIncomePerSample * 2; // since samples every 0.5s
 		}
 
-		hackText = document.querySelector(".MuiTypography-root.MuiTypography-body1.css-jtr2np")
-		moneyText = document.querySelector(".MuiTypography-root.MuiTypography-body1.css-1wj6ynj")
+		hackText = eval('document.querySelector(".MuiTypography-root.MuiTypography-body1.css-jtr2np")')
 		expGain = formatNumber(Math.floor(ns.getTotalScriptExpGain()))
-	  income = Math.floor(avgIncomePerSecond)
 		hackText.textContent = "Hack " + expGain + "/s "
-		if(income > 0) { moneyText.innerHTML = "Money $" + formatNumber(income) + "/s&nbsp;" }
-		karmastat = ns.heart.break()
-		bars = [...document.querySelectorAll('tr')].filter(row =>
-			row.querySelector('th.css-wv5f3v-cellNone > span.MuiLinearProgress-root')
-		);
-		if (bars.length < 6) return;
-		charismaBarRow = bars[5];
 
-		oldKarmaRow = document.getElementById('karma-row');
+		moneyText = eval('document.querySelector(".MuiTypography-root.MuiTypography-body1.css-1wj6ynj")')
+		income = Math.floor(avgIncomePerSecond)
+		if (income > 0) { moneyText.innerHTML = "Money $" + formatNumber(income) + "/s&nbsp;" }
+
+		intText = eval('document.querySelector(".MuiTypography-root.MuiTypography-body1.css-1kt61tv")')
+		intPercent = Math.floor(calculateSkillProgress(player.exp.intelligence))
+		intText.innerHTML = "Int " + intPercent + "%&nbsp;"
+
+		karmastat = ns.heart.break()
+		bars = [...eval('document.querySelectorAll("tr")')].filter(row =>
+			eval('row.querySelector("th.css-wv5f3v-cellNone > span.MuiLinearProgress-root")')
+		);
+		if (bars.length < 7) return;
+		charismaBarRow = bars[6];
+
+		oldKarmaRow = eval('document.getElementById("karma-row")');
 		if (oldKarmaRow) oldKarmaRow.remove();
 
 		formattedKarma = (karmastat < 0 ? '-' : '') + formatNumber(Math.abs(karmastat));
 
-		p = document.createElement('p');
+		p = eval('document.createElement("p")');
 		p.textContent = `Karma ${formattedKarma}`;
-		p.style.color = '#991919'
+		p.style.color = '#aa2a2a'
 		p.style.fontFamily = 'JetBrainsMono, "Courier New", monospace';
-		p.style.fontWeight = '600';
+		p.style.fontWeight = '400';
 		p.style.fontSize = '1rem';
-		p.style.lineHeight = '1.43';
-		p.style.margin = '0px 0 0 0';
+		p.style.lineHeight = '1.5';
+		p.style.margin = '0px';
 
-		newRow = document.createElement('tr');
+		newRow = eval('document.createElement("tr")');
 		newRow.id = 'karma-row';
 		newRow.style.border = 'none';
 
-		newCell = document.createElement('td');
+		newCell = eval('document.createElement("td")');
 		newCell.colSpan = charismaBarRow.children.length;
 		newCell.style.border = 'none';
 		newCell.style.padding = '0px 0px';
@@ -73,7 +108,6 @@ export async function main(ns) {
 
 		newRow.appendChild(newCell);
 		charismaBarRow.parentNode.insertBefore(newRow, charismaBarRow.nextSibling);
-
 
 		await ns.sleep(500)
 	}
